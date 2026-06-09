6000711d1bc0a2bec7c65371e01240c3d0a78e3c
shafikulnayem10 <inayem98@gmail.com>
2026-06-10 00:53:26 +0600

    added recruiter auth

--- src/lib/core/session.js
+++ src/lib/core/session.js
@@ -1,11 +1,23 @@
 
-import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { auth } from "../auth";
+import { headers } from "next/headers";
 
 export const getUserSession = async () => {
     const session = await auth.api.getSession({
-        headers: await headers() 
+        headers: await headers()
     })
 
     return session?.user || null;
+}
+
+export const requireRole = async(role) =>{
+    const user = await getUserSession()
+    if(!user){
+        redirect('/auth/signin')
+    }
+    if(user?.role !== role){
+        redirect('/unauthorized')
+    }
+    return user;
 }
