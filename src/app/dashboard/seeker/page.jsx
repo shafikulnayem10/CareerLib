bd160d9e89836dbb93b81772b8ca231f112d2de5
shafikulnayem10 <inayem98@gmail.com>
2026-06-10 00:09:34 +0600

    added job seeker dashboard

---
+++ src/app/dashboard/seeker/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2>Job Seeker Dashboard home page</h2>
+        </div>
+    );
+};
+
+export default page;
