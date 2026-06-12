7b9fad631f99096f4d7debfa8fbcc4e7f8de673c
shafikulnayem10 <inayem98@gmail.com>
2026-06-13 04:02:39 +0600

    added admin users page

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,11 @@
+import { adminClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
    
-    baseURL: process.env.BETTER_AUTH_URL
+    baseURL: process.env.BETTER_AUTH_URL,
+    plugins: [
+        adminClient()
+    ]
 })
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient()
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { admin } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
@@ -25,5 +26,9 @@ export const auth = betterAuth({
                 default: "seeker_free"
             }
         }
-    }
+    },
+      
+    plugins: [
+        admin()
+    ]
 });
